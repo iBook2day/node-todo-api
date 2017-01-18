8db3c1516d3631edb23e514ab5cf8ecfc03d017d
Liron Lichterman <BizBox@MacBook-Air-2.local>
2017-01-18 11:43:43 +0200

    adding server end points

---
+++ server/server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+
+
+//de - structuring
+var {
+    mongoose
+} = require('./db/mongoose.js');
+
+var {
+    Todo
+} = require('./models/todo.js');
+
+var {
+    User
+} = require('./models/user.js');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then(
+        (doc) => {
+            res.send(doc);
+        },
+        (err) => {
+            res.status(400).send(err);
+        });
+
+
+    console.log(req.body);
+});
+
+app.listen(3000, () => {
+    console.log("listen on port 3000")
+});
