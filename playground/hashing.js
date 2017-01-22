ee70c810838b16d8523a73b65886e835a99c8a8c
Liron Lichterman <BizBox@MacBook-Air-2.local>
2017-01-22 17:48:50 +0200

    crypt password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -4,16 +4,30 @@ const {
 
 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+})
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+var hashpass = '$2a$10$btpdQpquhBv08gIl2OLshe5vCekGsm2sCBZgzUsFDm15l8.NHo1LW';
+bcrypt.compare(password, hashpass, (err, res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
 
 // var message = 'I am user number';
 //
