3a6b6af2947c3fa36442a40fcb55c41947efdf3f
Liron Lichterman <BizBox@MacBook-Air-2.local>
2017-01-29 16:56:32 +0200

    add DELETE users/me/token

--- server/server.js
+++ server/server.js
@@ -180,6 +180,19 @@ app.get('/users/me', authenticate, (req, res) => {
 
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(
+        () => {
+            res.status(200).send()
+        },
+        () => {
+            res.status(400).send()
+        }
+
+    );
+
+})
+
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password'])
 
